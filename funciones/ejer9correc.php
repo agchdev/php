5c854d2c96877a99495285bd02934151822132a0
unknown <alex.chinchi02@gmail.com>
2024-11-11 11:43:27 +0100

    cambios

--- funciones/ejer9correc.php
+++ funciones/ejer9correc.php
@@ -8,7 +8,30 @@
 <body>
     <?php
         function get_ini_mes( int $mes, int $anio ){
+            $time = mktime(0,0,0,$mes, 1, $anio);
             
+            $diaSem = getdate($time)["wday"];
+
+            return $diaSem;
+        }
+
+        function get_dias_mes( int $mes, int $anio ){
+            $time = mktime(0,0,0,$mes, 1, $anio);
+            
+            $diaTot = intval(date("t", $time));
+
+            return $diaTot;
+        }
+
+        function impr_mes( int $mes, int $anio ){
+            $diaSem = get_ini_mes($mes, $anio);
+
+            $totIter = get_dias_mes($mes, $año) + $diaSem - 1;
+            echo "<table><tr><th>L</th><th>M</th><th>X</th><th>J</th><th>V</th><th>S</th><th>D</th></tr>";
+
+            for($i = 1; $i <= $totIter; $i++){
+                
+            }
         }
     ?>
 </body>
