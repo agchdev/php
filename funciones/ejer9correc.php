e8a00ed1d0b616452e570722fabcede593dcc293
unknown <alex.chinchi02@gmail.com>
2024-11-11 12:38:19 +0100

    cambios

--- funciones/ejer9correc.php
+++ funciones/ejer9correc.php
@@ -15,7 +15,7 @@
             return $diaSem;
         }
 
-        function get_dias_mes( int $mes, int $anio ){
+        function get_dias_mes( int $mes, int $anio){
             $time = mktime(0,0,0,$mes, 1, $anio);
             
             $diaTot = intval(date("t", $time));
@@ -27,17 +27,22 @@
             return '<td style="background-color: '.$color.'">'.$numDia.'</td>';
         }
 
-        function is_fest( int $dia, int $mes, int $anio){
+        function is_fest( int $dia, int $mes){
             $festivos = ["1-1", "6-1", "28-2", "1-5", "15-8", "12-10", "1-11", "6-12", "25-12"];
 
             $diaMes = "$dia-$mes";
             
-            array_search($diaMes, $festivos);
+            if(array_search($diaMes, $festivos)){
+                return true;
+            }else{
+                return false;
+            }
         }
 
-        function impr_mes( int $mes, int $anio ){
+        function impr_mes( int $mes, int $anio, string $cFSEM, string $cSEM, string $cFEST ){
             $diaSem = get_ini_mes($mes, $anio);
-
+            if ($diaSem == 0) $diaSem = 7;
+            
             $totIter = get_dias_mes($mes, $anio) + $diaSem - 1;
             $dias = 1;
 
@@ -47,8 +52,15 @@
                 if($i%7 == 1) echo "<tr>";
 
                 if( $i >= $diaSem ) {
-                    if( $i%7 >= 1 && $i%7 <= 5) echo impr_dias($dias++, "green");
-                    else echo impr_dias($dias++, "red");
+                    if(is_fest($dias,$mes)){
+                        echo impr_dias($dias++, $cFEST);
+                    }else{
+                        if( $i%7 >= 1 && $i%7 <= 5){
+                            echo impr_dias($dias++, $cSEM);
+                        }else{
+                            echo impr_dias($dias++, $cFSEM);
+                        }
+                    }
                 }
                 
                 else echo "<td></td>";
@@ -60,7 +72,54 @@
 
             
         }
-        impr_mes(11, 2024);
+
+        if(isset($_POST["Enviar"])){
+            impr_mes($_POST["mes"], 2024, $_POST["colorFSEM"], $_POST["colorSEM"], $_POST["colorFEST"] );
+        }else{
+    ?>
+    <form action="ejer9correc.php" method="post" enctype="multipart/form-data">
+        <select name="mes">
+            <option value=1 default>Enero</option>
+            <option value=2>Febrero</option>
+            <option value=3>Marzo</option>
+            <option value=4>Abril</option>
+            <option value=5>Mayo</option>
+            <option value=6>Junio</option>
+            <option value=7>Julio</option>
+            <option value=8>Agosto</option>
+            <option value=9>Septiembre</option>
+            <option value=10>Octubre</option>
+            <option value=11>Noviembre</option>
+            <option value=12>Diciembre</option>
+        </select>
+        <label for="colorFEST">Introduce el color del dia festivo</label>
+        <select name="colorFEST">
+            <option value="red" default>rojo</option>
+            <option value="blue">azul</option>
+            <option value="yellow">amarillo</option>
+            <option value="white">blanco</option>
+            <option value="purple">morado</option>
+        </select>
+        <label for="colorSEM">Introduce el color del dia de la semana</label>
+        <select name="colorSEM">
+            <option value="red" default>rojo</option>
+            <option value="blue">azul</option>
+            <option value="yellow">amarillo</option>
+            <option value="white">blanco</option>
+            <option value="purple">morado</option>
+        </select>
+        <label for="colorFSEM">Introduce el color de los fines de semana</label>
+        <select name="colorFSEM">
+            <option value="red" default>rojo</option>
+            <option value="blue">azul</option>
+            <option value="yellow">amarillo</option>
+            <option value="white">blanco</option>
+            <option value="purple">morado</option>
+        </select>
+        <input type="submit" name="Enviar" value="Enviar">
+    </form>
+    <?php
+    }
     ?>
 </body>
 </html>
