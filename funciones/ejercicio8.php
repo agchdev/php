43e08b33cace823efc9589b1edba9da3af90e2a9
unknown <alex.chinchi02@gmail.com>
2024-11-11 11:04:47 +0100

    cambios

--- funciones/ejercicio8.php
+++ funciones/ejercicio8.php
@@ -7,17 +7,44 @@
 </head>
 <body>
     <?php
-        if (isset($_POST["enviar"])) {
-            $fecha = $_POST["date"];
-            
-        }else{
-    ?>
-    <form action="ejercicio8.php" method="post" enctype="multipart/form-data">
-        <input type="date" name="fecha">
-        <input type="submit" name="enviar" value="enviar">
-    </form>
-    <?php
+        function dif_fecha( string $fecha ){
+            $timeFecha = strtotime($fecha);
+
+            $arrTime = getdate();
+            $timeHoy = mktime(0,0,0,$arrTime["mon"], $arrTime["mday"], $arrTime["year"]);
+
+            $dif = ($timeHoy - $timeFecha)/(24*3600);
+
+            return $dif;
         }
-    ?>
+
+        if(isset($_POST["env"])){
+            foreach($_POST as $key => $value){
+                if(strstr($key, "nom")) echo "Nombre: $value<br>";
+
+                if(preg_match("'^fecha'", $key)){
+                   $dias = dif_fecha($value); 
+                    $recargo = 0;
+
+                    if($dias > 0) $recargo = $dias * 5; 
+
+                    echo "Fecha: $value<br>Tiene un recargo de $recargo";
+                }
+            }
+
+        }
+        else{
+        ?>
+            <form action="#" method="post">
+                <input type="text" name="nom1" placeholder="Introduce el nombre">
+                <br>
+                <label for="fecha">Introduce la fecha de pago:</label>
+                <input type="date" name="fecha1">
+                <br>
+                <input type="submit" value="Enviar" name="env">
+            </form>
+    
+    
+        <?php } ?>
 </body>
 </html>
