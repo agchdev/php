66641d7c51d461d8e7730307eaf239b52022dac6
unknown <alex.chinchi02@gmail.com>
2024-11-18 12:47:44 +0100

    poo

---
+++ poo/objetos.php
@@ -0,0 +1,32 @@
+<?php
+    class persona{
+        public $nombre;
+        public $apellidos;
+        private $fecha_nac;
+
+        public function __construct ($n="", $a="", $fn="01/01/2000"){
+            $this->nombre = $n;
+            $this->apellidos = $a;
+            $this->fecha_nac = $fn;
+        }
+
+        public function nom_completo(){
+            $nom = $this->nombre." ".$this->apellidos;
+            return $nom;
+        }
+
+        public function set_nom($n){
+            $this->nombre = $n;
+        }
+
+        public function __toString(){
+            $str = "Me llamo ".$this->nom_completo()." y nací en ".$this->fecha_nac;
+            return $str;
+        }
+    }
+
+    $yo = new persona( "Érica", "Palomino");
+
+    echo $yo;
+
+?>
